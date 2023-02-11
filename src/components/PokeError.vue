e94663c63eb9aecba8fa569ac0139cf123be125b
Lucas Oliveira Vieira Cardoso <lucascardoso5263@gmail.com>
2023-02-11 03:53:25 -0300

    arrumar mensagem de erro no mobile

--- src/components/PokeError.vue
+++ src/components/PokeError.vue
@@ -1,3 +1,4 @@
 <template>
-    <div class="bg-red-600 rounded-xl text-white text-center font-bold p-4 w-4/12">Este Pokemon Não Existe!</div>
+    <div class="bg-red-600 rounded-xl text-white text-center font-bold w-6/12 p-4 lg:w-4/12">Este Pokemon Não Existe!
+    </div>
 </template>
