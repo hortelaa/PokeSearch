8b0ce372192e0d4464a7163eb282b6120e08435c
Lucas Oliveira Vieira Cardoso <lucascardoso5263@gmail.com>
2023-02-11 02:20:31 -0300

    Criar o Front End da Pág Principal com título e barra de busca

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,21 @@
 <template>
-  teste
+  <main class="flex flex-col py-12 gap-8 bg-gray-900 items-center justify-center min-h-screen max-w-full w-full">
+    <section class="w-full flex justify-center items-center">
+      <h1 class="font-bold text-5xl md:text-7xl text-white">Poke<span class="text-red-600">Search</span></h1>
+    </section>
+    <section class="w-full flex justify-center items-center">
+      <PokeBusca></PokeBusca>
+    </section>
+  </main>
 </template>
 
 <script>
+import PokeBusca from './components/PokeBusca.vue';
 
+export default {
+  name: "Main",
+  components: {
+    PokeBusca
+  },
+}
 </script>
---
+++ src/components/PokeBusca.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="relative group w-10/12 md:w-7/12 lg:w-5/12">
+        <input placeholder="Digite um nome de Pokemon aqui!"
+            class="bg-transparent pl-4 w-full hover:bg-gray-800 text-white placeholder:text-white/20 font-normal focus:border-0 focus:bg-gray-800 focus:outline-0 duration-300 hover:duration-300 ease-in-out shadow-gray-900 hover:border-gray-900 rounded-full border border-white/30 py-3">
+        <button class="absolute inset-y-0 r right-0 flex items-center pr-4 ">
+            <MagnifyingGlassIcon class="w-5 group-hover:text-white duration-300  h-5 text-white/30" />
+        </button>
+    </div>
+</template>
+
+<script>
+import { MagnifyingGlassIcon } from '@heroicons/vue/24/solid'
+
+export default {
+    name: "Barra de Busca",
+    components: {
+        MagnifyingGlassIcon
+    },
+}
+
+</script>
