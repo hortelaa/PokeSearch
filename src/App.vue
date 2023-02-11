c88cfc055a986cc1982a29512d2000778adac6f6
Lucas Oliveira Vieira Cardoso <lucascardoso5263@gmail.com>
2023-02-11 02:23:33 -0300

    adicionar funcionalidade a barra de busca ligando com a api

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@
       <h1 class="font-bold text-5xl md:text-7xl text-white">Poke<span class="text-red-600">Search</span></h1>
     </section>
     <section class="w-full flex justify-center items-center">
-      <PokeBusca></PokeBusca>
+      <PokeBusca @queryPokemon="queryPokemon"></PokeBusca>
     </section>
   </main>
 </template>
@@ -16,6 +16,29 @@ export default {
   name: "Main",
   components: {
     PokeBusca
+  },
+  data() {
+    return {
+
+    }
+  },
+  methods: {
+    async queryPokemon(query) {
+      fetch(`https://pokeapi.co/api/v2/pokemon-species/${query}`)
+        .then((res) => {
+          if (res.ok) {
+            this.error = false;
+            return res.json();
+          } else {
+            this.error = true;
+            console.log(this.error);
+          }
+        })
+        .then((data) => {
+          this.evo_url = data.evolution_chain.url
+          console.log(this.evo_url);
+        })
+    },
   },
 }
 </script>
--- src/components/PokeBusca.vue
+++ src/components/PokeBusca.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="relative group w-10/12 md:w-7/12 lg:w-5/12">
-        <input placeholder="Digite um nome de Pokemon aqui!"
+        <input placeholder="Digite um nome de Pokemon aqui!" v-model="query"
             class="bg-transparent pl-4 w-full hover:bg-gray-800 text-white placeholder:text-white/20 font-normal focus:border-0 focus:bg-gray-800 focus:outline-0 duration-300 hover:duration-300 ease-in-out shadow-gray-900 hover:border-gray-900 rounded-full border border-white/30 py-3">
-        <button class="absolute inset-y-0 r right-0 flex items-center pr-4 ">
+        <button @click="queryPokemon" class="absolute inset-y-0 r right-0 flex items-center pr-4 ">
             <MagnifyingGlassIcon class="w-5 group-hover:text-white duration-300  h-5 text-white/30" />
         </button>
     </div>
@@ -16,6 +16,11 @@ export default {
     components: {
         MagnifyingGlassIcon
     },
+    methods: {
+        queryPokemon() {
+            this.$emit('queryPokemon', this.query)
+        }
+    }
 }
 
 </script>
